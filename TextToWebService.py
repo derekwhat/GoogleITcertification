808bc7393a27a7e94c7a5fafa2ccac663448fc17
DerekWhat <derekho4.20@gmail.com>
2020-07-24 16:43:18 -0400

    completed submission for Week 2's assignment for the final course

--- TextToWebService.py
+++ TextToWebService.py
@@ -7,37 +7,47 @@ using requests
 import os
 import requests
 
-# fields = "title name date feedback".split()
-# src = r"/data/feedback"
-# site = r"http://<corpweb-external-IP>/feedback"
+fields = "title name date feedback".split()
+src = r"/data/feedback"
+externalIP = r"35.232.92.252"
+site = r"http://{}/feedback/".format(externalIP)
+print(site)
 
 
 def ProcessReviews(src, fields):
     files = os.listdir(src)
     reviews = []
     for file in files:
-        #get content of review
-        with open(file, 'r') as f:
-            lines = [line.rstip() for line in f.readlines()]
+        # get content of review
+        with open(os.path.join(src, file), 'r') as f:
+            lines = [line.rstrip() for line in f.readlines()]
 
             # populate dictionary
-            review = {field: line for line in lines for field in fields}
-            print("review: {}".format(review))
+            review = dict(zip(fields, lines))
+            for field in fields:
+                print("review {}: {}".format(fields[0], review[field]))
             reviews.append(review)
             f.close()
+    print("--"*10)
     return reviews
 
 
 def UploadReviews(dictOfReviews, site):
     for review in dictOfReviews:
-        # convert to Json??
-
+        print(review)
         response = requests.post(site, data=review)
+        print("status code: {}".format(response.status_code))
         response.raise_for_status()
 
 
-if "__name__" == "__main__":
-    fields = "title name date feedback".split()
-    src = r"/data/feedback"
-    site = r"http://<corpweb-external-IP>/feedback"
-    UploadReviews(ProcessReviews(src, fields), site)
+UploadReviews(ProcessReviews(src, fields), site)
+
+
+# if "__name__" == "__main__":
+#     fields = "title name date feedback".split()
+#     src = r"/data/feedback"
+#     externalIP = r"35.232.92.252"
+#     site = r"http://{}/feedback".format(externalIP)
+#     UploadReviews(ProcessReviews(src, fields), site)
+
+# student-01-5f8b2eead015@35.188.78.7
